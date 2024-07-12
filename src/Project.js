7a99095541e89f30507422afa6d8fb690847cbb7
AashishKr27 <ashish.21gcebds063@galgotiacollege.edu>
2024-07-13 01:29:02 +0530

    commit

--- src/Project.js
+++ src/Project.js
@@ -2,6 +2,7 @@
 import React from "react";
 import amazomClone from "./amazonClone.jpg";
 import ashishKumar from './ashishKumar.jpg'
+import sonarRock from './sonarRock.jpg'
 
 const Project = () => {
   return (
@@ -22,10 +23,10 @@ const Project = () => {
             <h2 className="lele">Portfolio</h2>
           </div>
         </a>
-        <a href="https://github.com/AashishKr27/Amazon-Clone" rel="noreferrer" target="_blank" className="bhr">
+        <a href="https://github.com/AashishKr27/Sonar-Rock-vs-Mine-Prediction" rel="noreferrer" target="_blank" className="bhr">
           <div className="project">
-            <img className="img" src={amazomClone} alt="amazon-clone" />
-            <h2 className="lele">Amazon Clone</h2>
+            <img className="img" src={sonarRock} alt="amazon-clone" />
+            <h2 className="lele">Sonar Rock vs Mine</h2>
           </div>
         </a>
         <a href="https://github.com/AashishKr27/Amazon-Clone" rel="noreferrer" target="_blank" className="bhr">
